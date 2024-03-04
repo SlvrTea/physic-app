c4a3c71514f25bcb65f0a72c43013344bfa4b77f
SlvrTea <art.bobr.ak@gmail.com>
2024-03-04 22:31:01 +0400

    Initial commit

---
+++ lib/game/game.dart
@@ -0,0 +1,52 @@
+import 'dart:io';
+import 'dart:math';
+
+import 'package:flame_forge2d/flame_forge2d.dart';
+import 'package:flutter/material.dart';
+import 'package:sensors_plus/sensors_plus.dart';
+import 'package:space/game/game_world.dart';
+
+import 'components/ball.dart';
+
+class AccelerometerGravityGame extends Forge2DGame {
+  AccelerometerGravityGame()
+      : super(gravity: Vector2(0, 10.0), world: AccelerometerGravityWorld());
+
+  @override
+  Color backgroundColor() {
+    return Colors.white;
+  }
+
+  List<Ball> balls = [];
+
+  void createBalls() {
+    balls = List.generate(
+      20,
+      (index) => Ball(Vector2.all(Random().nextDouble() * 10)),
+    );
+  }
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+    createBalls();
+    world.addAll(balls);
+    final double platformMultiplier = Platform.isIOS ? 1.5 : 3.0;
+    accelerometerEvents.listen((AccelerometerEvent event) {
+      final gravityVector = Vector2(
+        -event.x * 50 * platformMultiplier,
+        event.y * 80 * platformMultiplier,
+      );
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        updateObjectGravity(gravityVector);
+      });
+    });
+  }
+
+  void updateObjectGravity(Vector2 gravityVector) {
+    for (final ball in balls) {
+      final gravityForce = gravityVector;
+      ball.body.applyForce(gravityForce);
+    }
+  }
+}
---
+++ lib/game/game_world.dart
@@ -0,0 +1,14 @@
+import 'package:flame/components.dart';
+import 'package:flame_forge2d/flame_forge2d.dart';
+
+import 'components/boundaries.dart';
+
+class AccelerometerGravityWorld extends Forge2DWorld
+    with HasGameReference<Forge2DGame> {
+  @override
+  Future<void> onLoad() async {
+    super.onLoad();
+    final boundaries = createBoundaries(game);
+    addAll([...boundaries]);
+  }
+}
